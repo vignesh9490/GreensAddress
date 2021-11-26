52648db7c2da1f040496f353e5eaefb639c64129
vignesh9490 <vignesh.vicky378@gmail.com>
2021-11-26 16:30:44 +0530

    GreensAddress

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,30 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("omr");
+		
+   }
+	private void greensAdayar() {
+		System.out.println("Adayar");
+	}
+	private void greensTambaram() {
+		System.out.println("tambaram");
+
+	}
+	private void greensVelachery() {
+		System.out.println("velacherry");
+
+	}
+	private void greensAnnanagar() {
+		System.out.println("annanagar");
+	}
+	public static void main(String[] args) {
+		GreensTech g = new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelachery();
+		g.greensAnnanagar();
+	}
+	}
